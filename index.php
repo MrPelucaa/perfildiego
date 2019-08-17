d1427ec5280b86e9eb33ebaa7def19f9e2dc8435
MrPelucaa <diegosandovalramirez@gmail.com>
2019-08-17 17:02:55 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>El terrible de perfil</title>
+  </head>
+  <body style="background-image: url(fondo.jpg); background-size: cover;">
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <img src="diego.png" style="margin: 3em; margin-color:#f06359; " class="rounded mx-auto d-block" alt="">
+
+</div>
+</div>
+
+<div class="jumbotron" style="background-color: rgba(255,255,255,0.5);" >
+<div class="text-center" style="">
+  <h1 style="font-size: 3em;">Diego Sandoval Ramírez</h1>
+  <h4>Estudiante de Ingeniería Civil Informática</h4>
+  <h5>"Me pidieron hacer una pagina de presentacion y la hice tarde"</h5>
+  </div>
+  <div class="row justify-content-between" style="padding:1em;">
+     <div class="col-4">
+       <button type="button" class="btn btn-info">Linkned-in donde pretendo ser serio</button>
+     </div>
+     <div class="col-4">
+       <p>
+
+  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    Ver el codigo
+  </button>
+</p>
+</div>
+   </div>
+ </div>
+ <div class="collapse" id="collapseExample">
+   <div class="card card-body">
+<h3>Asi que quieres saber sobre mi codigo?</h3>
+<img src="https://media1.tenor.com/images/3cee627ab9f455a0f14739ba5edbf81a/tenor.gif?itemid=13499314" style="width:498px;"alt="">
+
+<!-- Aqui va todo el codigo que no pondre infinitamente-->
+<xmp>
+  <!doctype html>
+  <html lang="en">
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous">
+
+      <title>El terrible de perfil</title>
+    </head>
+    <body style="background-image: url(fondo.jpg); background-size: cover;">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img src="diego.png" style="margin: 3em; margin-color:#f06359; "
+         class="rounded mx-auto d-block" alt="">
+
+  </div>
+  </div>
+
+  <div class="jumbotron" style="background-color: rgba(255,255,255,0.5);" >
+  <div class="text-center" style="">
+    <h1 style="font-size: 3em;">Diego Sandoval Ramírez</h1>
+    <h4>Estudiante de Ingeniería Civil Informática</h4>
+    <h5>"Me pidieron hacer una pagina de presentacion y la hice tarde"</h5>
+    </div>
+    <div class="row justify-content-between">
+       <div class="col-4">
+         <button type="button" class="btn btn-info">Linkned-in donde pretendo ser serio</button>
+       </div>
+       <div class="col-4">
+         <p>
+
+    <button class="btn btn-primary" type="button" data-toggle="collapse"
+    data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+      Ver el codigo
+    </button>
+  </p>
+  </div>
+     </div>
+   </div>
+   <div class="collapse" id="collapseExample">
+     <div class="card card-body">
+  <h3>Asi que quieres saber sobre mi codigo?</h3>
+  <img src="https://media1.tenor.com/images/3cee627ab9f455a0f14739ba5edbf81a/tenor.gif?itemid=13499314"
+  style="width:498px;"alt="">
+  <pre> <code>
+  <!-- Aqui va todo el codigo que no pondre infinitamente-->
+
+  </code>
+  </pre>
+     </div>
+   </div>
+
+
+  </div>
+
+
+  </div>
+
+
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity=
+      "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+      crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+      crossorigin="anonymous"></script>
+    </body>
+  </html>
+</xmp>
+   </div>
+ </div>
+
+
+</div>
+
+
+</div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
